1cdb6bfcbe56afdd63f7b8e75870347224bb19d6
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-09-12 15:46:00 -0700

    Set isTripleShotActive variable and remove when necessary

--- EpicLoot/src/Magic/MagicItemEffects/MultiShot.cs
+++ EpicLoot/src/Magic/MagicItemEffects/MultiShot.cs
@@ -18,11 +18,17 @@ namespace EpicLoot.MagicItemEffects
         public static void Attack_FireProjectileBurst_Prefix(Attack __instance)
         {
             if (__instance?.GetWeapon() == null || __instance.m_character == null || !__instance.m_character.IsPlayer())
+            {
                 return;
+            }
+               
 
             var weaponDamage = __instance.GetWeapon()?.GetDamage();
             if (!weaponDamage.HasValue)
+            {
                 return;
+            }
+                
 
             var player = (Player)__instance.m_character;
             var doubleShot = player.HasActiveMagicEffect(MagicEffectType.DoubleMagicShot);
@@ -30,6 +36,8 @@ namespace EpicLoot.MagicItemEffects
 
             if (tripleShot)
             {
+                isTripleShotActive = true;
+                
                 if (__instance.m_projectileAccuracy < 2)
                 {
                     __instance.m_projectileAccuracy = 2;
@@ -37,7 +45,12 @@ namespace EpicLoot.MagicItemEffects
 
                 __instance.m_projectiles = 3;
             }
-            else if (doubleShot)
+            else
+            {
+                isTripleShotActive = false;
+            }
+            
+            if (doubleShot)
             {
                 if (__instance.m_projectileAccuracy < 2)
                 {
