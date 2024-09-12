a21b532315f05a12626340d37fb68b91c5fbdf63
jneb802 <73610029+jneb802@users.noreply.github.com>
2024-09-12 14:40:43 -0700

    Reduce nesting

--- EpicLoot/src/Magic/MagicItemEffects/ModifySummonDamage.cs
+++ EpicLoot/src/Magic/MagicItemEffects/ModifySummonDamage.cs
@@ -13,111 +13,113 @@ public class ModifySummonDamage
     {
         public static void Prefix(Attack __instance)
         {
-            if (__instance.m_character is Player player 
-                    && MagicEffectsHelper.HasActiveMagicEffect(player, __instance.m_weapon, MagicEffectType.ModifySummonDamage)
-                    && __instance.m_attackProjectile != null)
+            if (!(__instance.m_character is Player player) || 
+                !MagicEffectsHelper.HasActiveMagicEffect(player, __instance.m_weapon, MagicEffectType.ModifySummonDamage) ||
+                __instance.m_attackProjectile == null)
             {
-                // Debug.Log("Item with name " + __instance.m_weapon.m_shared.m_name + " has ModifySummonDamage and AttackProjectile is not null");
-                float modifier = 1 + player.GetTotalActiveMagicEffectValue(MagicEffectType.ModifySummonDamage, 0.01f);
-                
-                var spawnProjectile = __instance.m_attackProjectile;
-                if (spawnProjectile.TryGetComponent<SpawnAbility>(out var spawnAbility))
+                return;
+            }
+
+            float modifier = 1 + player.GetTotalActiveMagicEffectValue(MagicEffectType.ModifySummonDamage, 0.01f);
+            var spawnProjectile = __instance.m_attackProjectile;
+
+            if (!spawnProjectile.TryGetComponent<SpawnAbility>(out var spawnAbility))
+            {
+                return;
+            }
+
+            var spawnPrefab = spawnAbility.m_spawnPrefab[0];
+
+            if (spawnPrefab == null || !spawnPrefab.TryGetComponent<Humanoid>(out var humanoid))
+            {
+                return;
+            }
+
+            GameObject[] randomWeapons = humanoid.m_randomWeapon;
+            if (randomWeapons != null)
+            {
+                if (!originalDamages.ContainsKey(humanoid))
                 {
-                    // Debug.Log("Got spawnAbility with name: " + spawnAbility);
-                    var spawnPrefab = spawnAbility.m_spawnPrefab[0];
-                    if (spawnPrefab != null)
+                    originalDamages[humanoid] = new Dictionary<ItemDrop, HitData.DamageTypes>();
+                }
+
+                foreach (var weapon in randomWeapons)
+                {
+                    if (weapon.TryGetComponent(out ItemDrop itemDrop))
                     {
-                        // Debug.Log("Got spawnPrefab with name: " + spawnPrefab);
-                        if (spawnPrefab.TryGetComponent<Humanoid>(out var humanoid))
+                        var itemDropDamages = itemDrop.m_itemData.m_shared.m_damages;
+
+                        if (!originalDamages[humanoid].ContainsKey(itemDrop))
                         {
-                            GameObject[] randomWeapons = humanoid.m_randomWeapon;
-                            if (randomWeapons != null)
-                            {
-                                if (!originalDamages.ContainsKey(humanoid))
-                                {
-                                    originalDamages[humanoid] = new Dictionary<ItemDrop, HitData.DamageTypes>();
-                                }
-                                
-                                foreach (var weapon in randomWeapons)
-                                {
-                                    if (weapon.TryGetComponent(out ItemDrop itemDrop))
-                                    {
-                                        // Debug.Log("Found ItemDrop component on: " + weapon.name);
-                                        var itemDropDamages = itemDrop.m_itemData.m_shared.m_damages;
-                                        
-                                        if (!originalDamages[humanoid].ContainsKey(itemDrop))
-                                        {
-                                            originalDamages[humanoid][itemDrop] = itemDropDamages;
-                                        }
-                                        
-                                        // Debug.Log("Item with name: " + weapon.name + "has total damage with value:" + itemDropDamages.GetTotalDamage());
-                                        itemDropDamages.Modify(modifier);
-                                        // Debug.Log("Item with name: " + weapon.name + "has as modified total damage with value:" + itemDropDamages.GetTotalDamage());
-                                    }
-                                }
-                            }
-                            
-                            GameObject[] defaultItems = humanoid.m_defaultItems;
-                            if (defaultItems != null)
-                            {
-                                if (!originalDamages.ContainsKey(humanoid))
-                                {
-                                    originalDamages[humanoid] = new Dictionary<ItemDrop, HitData.DamageTypes>();
-                                }
-                                
-                                foreach (var weapon in defaultItems)
-                                {
-                                    if (weapon.TryGetComponent(out ItemDrop itemDrop))
-                                    {
-                                        // Debug.Log("Found ItemDrop component on: " + weapon.name);
-                                        var itemDropDamages = itemDrop.m_itemData.m_shared.m_damages;
-                                        
-                                        if (!originalDamages[humanoid].ContainsKey(itemDrop))
-                                        {
-                                            originalDamages[humanoid][itemDrop] = itemDropDamages;
-                                        }
-                                        
-                                        // Debug.Log("Item with name: " + weapon.name + "has total damage with value:" + itemDropDamages.GetTotalDamage());
-                                        itemDropDamages.Modify(modifier);
-                                        // Debug.Log("Item with name: " + weapon.name + "has as modified total damage with value:" + itemDropDamages.GetTotalDamage());
-                                    }
-                                }
-                            }
+                            originalDamages[humanoid][itemDrop] = itemDropDamages;
                         }
+
+                        itemDropDamages.Modify(modifier);
+                    }
+                }
+            }
+
+            GameObject[] defaultItems = humanoid.m_defaultItems;
+            if (defaultItems != null)
+            {
+                if (!originalDamages.ContainsKey(humanoid))
+                {
+                    originalDamages[humanoid] = new Dictionary<ItemDrop, HitData.DamageTypes>();
+                }
+
+                foreach (var weapon in defaultItems)
+                {
+                    if (weapon.TryGetComponent(out ItemDrop itemDrop))
+                    {
+                        var itemDropDamages = itemDrop.m_itemData.m_shared.m_damages;
+
+                        if (!originalDamages[humanoid].ContainsKey(itemDrop))
+                        {
+                            originalDamages[humanoid][itemDrop] = itemDropDamages;
+                        }
+
+                        itemDropDamages.Modify(modifier);
                     }
                 }
             }
         }
-
+        
         public static void Postfix(Attack __instance)
         {
-            if (__instance.m_attackProjectile != null)
+            if (__instance.m_attackProjectile == null)
             {
-                var spawnProjectile = __instance.m_attackProjectile;
-                if (spawnProjectile.TryGetComponent<SpawnAbility>(out var spawnAbility))
-                {
-                    var spawnPrefab = spawnAbility.m_spawnPrefab[0];
-                    if (spawnPrefab != null)
-                    {
-                        if (spawnPrefab.TryGetComponent<Humanoid>(out var humanoid))
-                        {
-                            if (originalDamages.TryGetValue(humanoid, out var itemDropDamages))
-                            {
-                                foreach (var kvp in itemDropDamages)
-                                {
-                                    if (kvp.Key.TryGetComponent(out ItemDrop itemDrop))
-                                    {
-                                        var originalDamage = kvp.Value;
-                                        itemDrop.m_itemData.m_shared.m_damages = originalDamage;
-                                    }
-                                }
+                return;
+            }
 
-                                originalDamages.Remove(humanoid); // Clean up the dictionary
-                            }
-                        }
-                    }
+            var spawnProjectile = __instance.m_attackProjectile;
+
+            if (!spawnProjectile.TryGetComponent<SpawnAbility>(out var spawnAbility))
+            {
+                return;
+            }
+
+            var spawnPrefab = spawnAbility.m_spawnPrefab[0];
+
+            if (spawnPrefab == null || !spawnPrefab.TryGetComponent<Humanoid>(out var humanoid))
+            {
+                return;
+            }
+
+            if (!originalDamages.TryGetValue(humanoid, out var itemDropDamages))
+            {
+                return;
+            }
+
+            foreach (var kvp in itemDropDamages)
+            {
+                if (kvp.Key.TryGetComponent(out ItemDrop itemDrop))
+                {
+                    var originalDamage = kvp.Value;
+                    itemDrop.m_itemData.m_shared.m_damages = originalDamage;
                 }
             }
+
+            originalDamages.Remove(humanoid);
         }
     }
 }
