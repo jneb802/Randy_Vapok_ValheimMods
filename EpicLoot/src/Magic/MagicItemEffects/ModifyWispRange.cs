3e68592b59b69c7fc43a71cf7f93a4e6ce18ae7a
OrianaVenture <107574865+OrianaVenture@users.noreply.github.com>
2024-10-23 12:25:31 -0500

    Attempt to fix ModifyWispRange for multiplayer (needs testing).

--- EpicLoot/src/Magic/MagicItemEffects/ModifyWispRange.cs
+++ EpicLoot/src/Magic/MagicItemEffects/ModifyWispRange.cs
@@ -3,38 +3,81 @@ using UnityEngine;
 
 namespace EpicLoot.MagicItemEffects
 {
-    [HarmonyPatch(typeof(SE_Demister), nameof(SE_Demister.UpdateStatusEffect))]
-    public static class ModifyWispRange_SE_Demister_UpdateStatusEffect_Patch
+    public class ModifyWispRange
     {
-        private static float _originalEndRange = -1f;
-        
-        public static void Prefix(SE_Demister __instance)
+        public const string DEMISTER_ZDO = "el-demist";
+
+        public static void SetDemister(ref Demister demister, float magicEffectValue)
         {
-            if (!__instance.m_character.IsPlayer() || __instance.m_ballPrefab == null)
+            if (demister == null || magicEffectValue <= 0f)
             {
                 return;
             }
-            
-            var forceField = __instance.m_ballPrefab.GetComponentInChildren<ParticleSystemForceField>();
+
+            var forceField = demister.GetComponent<ParticleSystemForceField>();
             if (forceField == null)
             {
                 return;
             }
-            
-            if (_originalEndRange < 0)
+
+            //EpicLoot.LogErrorForce("Setting Demister range multiplier!");
+            forceField.endRange *= (1 + magicEffectValue);
+        }
+    }
+
+    [HarmonyPatch(typeof(SE_Demister), nameof(SE_Demister.UpdateStatusEffect))]
+    public static class ModifyWispRange_SE_Demister_UpdateStatusEffect_Patch
+    {
+        public static void Prefix(SE_Demister __instance, ref bool __state)
+        {
+            if (!__instance.m_character.IsPlayer())
             {
-                _originalEndRange = forceField.endRange;
+                __state = false;
+                return;
             }
-            
-            Player player = (Player)__instance.m_character;
-            if (player.HasActiveMagicEffect(MagicEffectType.ModifyWispRange, out float effectValue, 0.01f))
+
+            __state = __instance.m_ballInstance == null;
+        }
+
+        public static void Postfix(SE_Demister __instance, ref bool __state)
+        {
+            if (!__state || __instance.m_ballInstance == null)
             {
-                forceField.endRange = _originalEndRange * (1 + effectValue);
+                return;
             }
-            else
+
+            // First time set up
+            var nview = __instance.m_ballInstance.GetComponent<ZNetView>();
+            if (nview != null && nview.GetZDO() != null && nview.GetZDO().IsOwner())
             {
-                forceField.endRange = _originalEndRange;
+                Player player = (Player)__instance.m_character;
+                if (player.HasActiveMagicEffect(MagicEffectType.ModifyWispRange, out float effectValue, 0.01f))
+                {
+                    //EpicLoot.LogErrorForce("Setting Demister range on zdo!");
+                    nview.GetZDO().Set(ModifyWispRange.DEMISTER_ZDO, effectValue);
+
+                    var demister = __instance.m_ballInstance.GetComponentInChildren<Demister>();
+                    ModifyWispRange.SetDemister(ref demister, effectValue);
+                }
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(Demister), nameof(Demister.Awake))]
+    public static class ModifyWispRange_Demister_Awake_Patch
+    {
+        public static void Postfix(Demister __instance)
+        {
+            var nview = __instance.transform.root.GetComponent<ZNetView>();
+
+            if (nview == null || nview.GetZDO() == null)
+            {
+                return;
+            }
+
+            float magicEffectValue = nview.GetZDO().GetFloat(ModifyWispRange.DEMISTER_ZDO, 0f);
+
+            ModifyWispRange.SetDemister(ref __instance, magicEffectValue);
         }
     }
 }
