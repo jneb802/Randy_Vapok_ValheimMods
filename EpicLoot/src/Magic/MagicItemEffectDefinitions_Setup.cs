118cd3917113e403683c32992950033b0b4443c5
warp <73610029+jneb802@users.noreply.github.com>
2025-07-05 15:27:12 -0700

    Add ModifyNoise magic effect

--- EpicLoot/src/Magic/MagicItemEffectDefinitions_Setup.cs
+++ EpicLoot/src/Magic/MagicItemEffectDefinitions_Setup.cs
@@ -57,6 +57,7 @@ namespace EpicLoot
         public static string AddPhysicalResistancePercentage = nameof(AddPhysicalResistancePercentage);
         public static string ModifyMovementSpeed = nameof(ModifyMovementSpeed);
         public static string ModifySprintStaminaUse = nameof(ModifySprintStaminaUse);
+        public static string ModifyNoise = nameof(ModifyNoise);
         public static string ModifyDodgeStaminaUse = nameof(ModifyDodgeStaminaUse);
         public static string ModifyJumpStaminaUse = nameof(ModifyJumpStaminaUse);
         public static string ModifyAttackStaminaUse = nameof(ModifyAttackStaminaUse);
